7e848c55c0e42bd2480aea94fea33642c65a8a70
WhaeaM <136761145+WhaeaM@users.noreply.github.com>
2026-02-12 15:10:16 +1300

    wef

---
+++ test.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+
+class Converter:
+    """
+    Temperature conversion tool (°C to °F or °F to °C)
+    """
+
+    def __init__(self):
+        """
+        Temperature converter GUI
+        """
+
+        self.temp_frame = Frame(padx=10, pady=10)
+        self.temp_frame.grid()
+
+        self.temp_heading = Label(self.temp_frame,
+                                  text="Temperature Convertor"
+                                  )
+        self.temp_heading.grid(row=0)
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Temperature Converter")
+    Converter()
+    root.mainloop()
+    
